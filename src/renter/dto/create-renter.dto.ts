b991e6bb5653cd1649d0ebdcc0738d6f5011a3a4
Jerónimo Gascón Sánchez <jerogassan@gmail.com>
2025-02-02 11:43:48 +0100

    A - Renter DTO

--- src/renter/dto/create-renter.dto.ts
+++ src/renter/dto/create-renter.dto.ts
@@ -1,1 +1,38 @@
-export class CreateRenterDto {}
+import { Transform } from 'class-transformer';
+import {
+  IsEmail,
+  IsMobilePhone,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+import { regexEnum } from 'src/utils/regex.enum';
+
+export class CreateRenterDto {
+  @IsString()
+  @MinLength(1)
+  @Transform(({ value }) => (value as string).toLowerCase().trim())
+  name: string;
+
+  @IsEmail()
+  @Transform(({ value }) => (value as string).toLowerCase().trim())
+  email: string;
+
+  @IsString()
+  @MinLength(8)
+  @MaxLength(20)
+  @Matches(new RegExp(regexEnum.PASSWORD_PATTERN), {
+    message:
+      'Password must have an uppercase letter, a lowercase letter, and a number',
+  })
+  password: string;
+
+  @IsMobilePhone()
+  phone: number;
+
+  @IsString()
+  @Transform(({ value }) => (value as string).toLowerCase().trim())
+  @Matches(new RegExp(regexEnum.DNI_PATTERN))
+  dni: string;
+}
